052ff77ac3ebe840375868b230dff584941ca2ea
KLaszlo24 <krajczlaci234@gmail.com>
2024-12-13 11:05:15 +0100

    F01 feladat elkészítve

--- órai munka/Program.cs
+++ órai munka/Program.cs
@@ -8,9 +8,24 @@ namespace órai_munka
 {
 	internal class Program
 	{
+
+		static Random rnd = new Random();
 		static void Main(string[] args)
 		{
-
+			F01();
 		}
+
+		static void F01()
+		{
+			
+			int[] szam = new int[20];
+            Console.WriteLine("Változtatott számok: ");
+            for (int i = 0; i < szam.Length; i++)
+			{
+				int random = rnd.Next(0,55);
+				szam[i] += random;
+				Console.Write(szam[i] + ",");
+            }
+        }
 	}
 }
