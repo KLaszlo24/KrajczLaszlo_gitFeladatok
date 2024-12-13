5c4c1a1ee72a733404dfa11f2951d668e2f88c62
KLaszlo24 <krajczlaci234@gmail.com>
2024-12-13 11:36:45 +0100

    F02 feladat elkészítve

--- órai munka/Program.cs
+++ órai munka/Program.cs
@@ -12,8 +12,14 @@ namespace órai_munka
 		static Random rnd = new Random();
 		static void Main(string[] args)
 		{
+			int[] szamok = { 12, 14, 15, 20, 45, 88, 98, 35 };
+
 			F01();
-		}
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Ennyi páros szám van: " + F02(szamok));
+        }
 
 		static void F01()
 		{
@@ -27,5 +33,19 @@ namespace órai_munka
 				Console.Write(szam[i] + ",");
             }
         }
+
+		static int F02(int[] tomb2)
+		{
+			Console.WriteLine("Második feladat:");
+			int szamlalo=0;
+			for (int i = 0;i < tomb2.Length; i++)
+			{
+				if (tomb2[i] %2 == 0)
+				{
+					szamlalo++;
+				}
+			}
+			return szamlalo;
+		}
 	}
 }
