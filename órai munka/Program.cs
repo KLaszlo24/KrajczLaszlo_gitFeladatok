cfbf08f9144fbca88874e88e08a9d71121fb7abf
KLaszlo24 <krajczlaci234@gmail.com>
2024-12-13 10:33:14 +0100

    Add project files.

---
+++ órai munka/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace órai_munka
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
